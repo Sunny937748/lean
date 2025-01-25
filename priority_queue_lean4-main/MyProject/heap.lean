664c675ce9bb944eaf7c0a1c91b4a9bade72cfbb
Sunny937748 <119325508+Sunny937748@users.noreply.github.com>
2025-01-25 18:01:17 +0100

    Update heap.lean

--- priority_queue_lean4-main/MyProject/heap.lean
+++ priority_queue_lean4-main/MyProject/heap.lean
@@ -1,151 +1,282 @@
-git config --global url."https://mirrors.ustc.edu.cn/".insteadOf "https://github.com/"
 import Mathlib.Data.Real.Basic
 import Mathlib.Tactic
 
--- 定义二叉树的结构
 inductive BinaryTree (α : Type) [Ord α] where
+  | empty : BinaryTree α
   | leaf : α → BinaryTree α
-  | left : α → BinaryTree α → BinaryTree α
-  | right : α → BinaryTree α → BinaryTree α
   | node : α → BinaryTree α → BinaryTree α → BinaryTree α
 
-namespace BinaryTree
-
-variables {α : Type} [Ord α]
-
-/-- 获取二叉树的节点值 -/
-def get_value : BinaryTree α → α
-  | BinaryTree.leaf v => v
-  | BinaryTree.left v _ => v
-  | BinaryTree.right v _ => v
-  | BinaryTree.node v _ _ => v
-
-/-- 判断是否为空堆 -/
-def is_empty : BinaryTree α → Bool
-  | BinaryTree.leaf _ => false
+def is_empty {α : Type} [Ord α] : BinaryTree α → Bool
+  | BinaryTree.empty => true
   | _ => false
 
-/-- 计算二叉树的大小（节点数） -/
-def size : BinaryTree α → Nat
+def b_size {α : Type} [Ord α] : BinaryTree α → Nat
+  | BinaryTree.empty => 0
   | BinaryTree.leaf _ => 1
-  | BinaryTree.left _ l => 1 + size l
-  | BinaryTree.right _ r => 1 + size r
-  | BinaryTree.node _ l r => 1 + size l + size r
+  | BinaryTree.node _ l r => 1 + b_size l + b_size r
 
-/-- 计算二叉树的深度（高度） -/
-def depth : BinaryTree α → Nat
+def b_depth {α : Type}[Ord α] : BinaryTree α → Nat
+  | BinaryTree.empty => 0
   | BinaryTree.leaf _ => 1
-  | BinaryTree.left _ l => 1 + depth l
-  | BinaryTree.right _ r => 1 + depth r
-  | BinaryTree.node _ l r => 1 + max (depth l) (depth r)
+  | BinaryTree.node _ l r => 1 + max (b_depth l) (b_depth r)
 
-/-- 判断二叉树是否为完全二叉树 -/
-def is_complete_binary_tree : BinaryTree α → Bool
-  | BinaryTree.leaf _ => true
-  | BinaryTree.left _ l => depth l = 1
-  | BinaryTree.right _ _ => false
-  | BinaryTree.node _ l r =>
-      depth l >= depth r && size l >= size r && is_complete_binary_tree l && is_complete_binary_tree r
+--层次遍历（BFS）
+--检查完全二叉树是否也是平衡树
+--判断是否为二叉搜索树
 
-/-- 先序遍历二叉树 -/
-def preorder : BinaryTree α → List α
+--查找最大值
+def get_max_value {α : Type} [Ord α] : BinaryTree α → Option α
+  | BinaryTree.empty => none
+  | BinaryTree.leaf v => some v
+  | BinaryTree.node _ _ r =>
+    match get_max_value r with
+    | some max => some max
+    | none => none
+
+--翻转二叉树
+def invert_binary_tree {α : Type} [Ord α] : BinaryTree α → BinaryTree α
+  | BinaryTree.empty => BinaryTree.empty
+  | BinaryTree.leaf v => BinaryTree.leaf v
+  | BinaryTree.node v l r => BinaryTree.node v (invert_binary_tree r) (invert_binary_tree l)
+
+--后序遍历
+def postorder {α : Type} [Ord α] : BinaryTree α → List α
+  | BinaryTree.empty => []
   | BinaryTree.leaf v => [v]
-  | BinaryTree.left v l => v :: preorder l
-  | BinaryTree.right v r => v :: preorder r
+  | BinaryTree.node v l r => postorder l ++ postorder r ++ [v]
+
+--中序遍历 
+def inorder {α : Type} [Ord α] : BinaryTree α → List α
+  | BinaryTree.empty => []
+  | BinaryTree.leaf v => [v]
+  | BinaryTree.node v l r => inorder l ++ [v] ++ inorder r
+--计算二叉树的叶子节点数
+def count_leaves {α : Type} [Ord α] : BinaryTree α → Nat
+  | BinaryTree.empty => 0
+  | BinaryTree.leaf _ => 1
+  | BinaryTree.node _ l r => count_leaves l + count_leaves r
+
+--镜像二叉树
+def mirror {α : Type} [Ord α] : BinaryTree α → BinaryTree α
+  | BinaryTree.empty => BinaryTree.empty
+  | BinaryTree.leaf v => BinaryTree.leaf v
+  | BinaryTree.node v l r => BinaryTree.node v (mirror r) (mirror l)
+
+--统计完全二叉树的节点数
+def count_nodes {α : Type} [Ord α] : BinaryTree α → Nat
+  | BinaryTree.empty => 0
+  | BinaryTree.leaf _ => 1
+  | BinaryTree.node _ l r =>
+    if b_depth l == b_depth r then
+      2 ^ b_depth l - 1
+    else
+      1 + count_nodes l + count_nodes r
+
+--将二叉树转换为列表
+def flatten_to_list {α : Type} [Ord α] : BinaryTree α → List α :=
+  inorder
+
+--路径搜索
+def find_all_paths {α : Type} [Ord α] : BinaryTree α → List (List α)
+  | BinaryTree.empty => []
+  | BinaryTree.leaf v => [[v]]
+  | BinaryTree.node v l r =>
+    let leftPaths := find_all_paths l
+    let rightPaths := find_all_paths r
+    (leftPaths ++ rightPaths).map (fun path => v :: path)
+
+--求二叉树的直径
+def diameter {α : Type} [Ord α] : BinaryTree α → Nat
+  | BinaryTree.empty => 0
+  | BinaryTree.leaf _ => 0
+  | BinaryTree.node _ l r =>
+    let leftDepth := b_depth l
+    let rightDepth := b_depth r
+    let leftDiameter := diameter l
+    let rightDiameter := diameter r
+    max (leftDepth + rightDepth) (max leftDiameter rightDiameter)
+
+--计算所有路径的长度
+def sum_of_path_lengths {α : Type} [Ord α] : BinaryTree α → Nat
+  | BinaryTree.empty => 0
+  | t =>
+    let rec helper (t : BinaryTree α) (depth : Nat) : Nat :=
+      match t with
+      | BinaryTree.empty => 0
+      | BinaryTree.leaf _ => depth
+      | BinaryTree.node _ l r => helper l (depth + 1) + helper r (depth + 1)
+    helper t 0
+
+
+def is_complete_binary_tree {α : Type} [Ord α] : BinaryTree α → Bool
+  | BinaryTree.empty => true
+  | BinaryTree.leaf _ => true
+  | BinaryTree.node _ l r =>
+    if b_depth l == b_depth r
+    then if b_size l == 2 ^ (b_depth l) - 1
+      then  is_complete_binary_tree r
+      else false
+    else b_depth l == b_depth r + 1
+    && is_complete_binary_tree l
+    && is_complete_binary_tree r
+
+def insert_into_binary_tree {α : Type} [Ord α] : α → BinaryTree α → BinaryTree α
+  | x, BinaryTree.empty => BinaryTree.leaf x
+  | x, BinaryTree.leaf y => BinaryTree.node y (BinaryTree.leaf x) (BinaryTree.empty)
+  | x, BinaryTree.node y l r =>
+    if b_size l < 2 ^ (b_depth l) - 1 then BinaryTree.node y (insert_into_binary_tree x l) r
+    else
+      if b_size r < 2 ^ (b_depth l) - 1 then BinaryTree.node y l (insert_into_binary_tree x r)
+      else BinaryTree.node y (insert_into_binary_tree x l) r
+
+def array_to_heap {α : Type} [Ord α] (arr : Array α) : BinaryTree α :=
+  arr.foldl (λ acc x => insert_into_binary_tree x acc) BinaryTree.empty
+
+def array_to_max_heap (arr : Array Nat) : BinaryTree Nat :=
+  let sortedArr := arr.qsort (· > ·)
+  array_to_heap sortedArr
+
+def array_to_min_heap (arr : Array Nat) : BinaryTree Nat :=
+  let sortedArr := arr.qsort (· < ·)
+  array_to_heap sortedArr
+
+def preorder {α : Type}[Ord α] : BinaryTree α → List α
+  | BinaryTree.empty => []
+  | BinaryTree.leaf v => [v]
   | BinaryTree.node v l r => v :: preorder l ++ preorder r
 
-/-- 判断二叉树是否为小顶堆 -/
-def is_min_heap : BinaryTree α → Bool :=
-  λ t => is_complete_binary_tree t &&
-         match t with
-         | leaf _ => true
-         | left v l => get_value l >= v && is_min_heap l
-         | right v r => get_value r >= v && is_min_heap r
-         | node v l r => get_value l >= v && get_value r >= v && is_min_heap l && is_min_heap r
+def get_value : BinaryTree Nat → Nat
+  | BinaryTree.empty => panic! "Cannot get value from an empty node"
+  | BinaryTree.leaf v => v
+  | BinaryTree.node v _ _ => v
 
-/-- 合并两个小顶堆 -/
-def merge : BinaryTree α → BinaryTree α → BinaryTree α
-  | t1@(leaf v1), t2@(leaf v2) =>
-    if v1 ≤ v2 then left v1 t2 else left v2 t1
-  | t1@(leaf v1), t2@(left v2 l2) =>
-    if v1 ≤ v2 then left v1 t2 else left v2 (merge t1 l2)
-  | t1@(leaf v1), t2@(right v2 r2) =>
-    if v1 ≤ v2 then right v1 t2 else right v2 (merge t1 r2)
-  | t1@(leaf v1), t2@(node v2 l2 r2) =>
-    if v1 ≤ v2 then node v1 t2 (leaf v1) else node v2 (merge t1 l2) r2
-  | t1@(node v1 l1 r1), t2@(node v2 l2 r2) =>
-    if v1 ≤ v2 then node v1 l1 (merge r1 t2) else node v2 l2 (merge t1 r2)
-  | t1@(node v1 l1 r1), t2@(leaf v2) =>
-    if v1 ≤ v2 then node v1 l1 (merge r1 t2) else left v2 t1
-  | t1@(node v1 l1 r1), t2@(left v2 l2) =>
-    if v1 ≤ v2 then node v1 l1 (merge r1 t2) else left v2 (merge t1 l2)
-  | t1@(node v1 l1 r1), t2@(right v2 r2) =>
-    if v1 ≤ v2 then node v1 l1 (merge r1 t2) else right v2 (merge t1 r2)
-  | t1, t2 => t1 -- fallback
+def is_min : BinaryTree Nat → Bool
+  | BinaryTree.empty => true
+  | BinaryTree.leaf _ => true
+  | BinaryTree.node v l r =>
+    if is_empty l then true
+      else get_value l >= v
+    && if is_empty r then true
+      else get_value r >= v
+    && is_min l
+    && is_min r
 
-/-- 上溢：调整堆使根节点值小于子节点值 -/
-def bubble_up : BinaryTree α → BinaryTree α
+def is_max : BinaryTree Nat → Bool
+  | BinaryTree.empty => true
+  | BinaryTree.leaf _ => true
+  | BinaryTree.node v l r =>
+    if is_empty l then true
+      else get_value l <= v
+    && if is_empty r then true
+      else get_value r <= v
+    && is_max l
+    && is_max r
+
+def is_min_heap : BinaryTree Nat → Bool :=
+  λ t => is_complete_binary_tree t && is_min t
+
+def is_max_heap : BinaryTree Nat → Bool :=
+  λ t => is_complete_binary_tree t && is_max t
+
+def get_top {h : BinaryTree Nat} : Nat :=
+  get_value h
+
+def get_end_value : BinaryTree Nat → Nat
+  | BinaryTree.empty => panic! "Cannot get value from an empty node"
+  | BinaryTree.leaf v => v
+  | BinaryTree.node _ l r =>
+    if b_size l <= 2 ^ (b_depth l) - 1 && b_size r == 2 ^ (b_depth l) - 1 then get_end_value l
+    else get_end_value r
+
+def remove_last_node : BinaryTree Nat → BinaryTree Nat
+  | BinaryTree.empty => BinaryTree.empty
+  | BinaryTree.leaf v => BinaryTree.empty
+  | BinaryTree.node v l r =>
+    if b_size r > b_size l then
+      let newRight := remove_last_node r
+      BinaryTree.node v l newRight
+    else
+      let newLeft := remove_last_node l
+      BinaryTree.node v newLeft r
+
+def delete_top {h : BinaryTree Nat} : BinaryTree Nat :=
+  sorry
+
+set_option linter.unusedVariables false
+def min_bubble_down : BinaryTree Nat → BinaryTree Nat
+  | BinaryTree.empty => BinaryTree.empty
   | BinaryTree.leaf v => BinaryTree.leaf v
-  | BinaryTree.left v l =>
-    if v > get_value l then BinaryTree.left (get_value l) (bubble_up (BinaryTree.leaf v))
-    else BinaryTree.left v l
-  | BinaryTree.right v r =>
-    if v > get_value r then BinaryTree.right (get_value r) (bubble_up (BinaryTree.leaf v))
-    else BinaryTree.right v r
   | BinaryTree.node v l r =>
-    if v > get_value l && get_value l <= get_value r then
-      BinaryTree.node (get_value l) (bubble_up (BinaryTree.leaf v)) r
-    else if v > get_value r then
-      BinaryTree.node (get_value r) l (bubble_up (BinaryTree.leaf v))
-    else BinaryTree.node v l r
+   match l, r with
+    | BinaryTree.empty, _ => BinaryTree.node v l r
+    | BinaryTree.leaf lv, BinaryTree.empty =>
+      if v > lv then BinaryTree.node lv (BinaryTree.leaf v) r
+      else BinaryTree.node v l r
+    | BinaryTree.leaf lv, BinaryTree.leaf rv =>
+      if v > lv then BinaryTree.node lv (BinaryTree.leaf v) r
+      else if v > rv then BinaryTree.node rv l (BinaryTree.leaf v)
+      else BinaryTree.node v l r
+    | BinaryTree.leaf lv, BinaryTree.node rv rl rr => BinaryTree.node v l r
+    | BinaryTree.node lv ll lr, BinaryTree.empty => BinaryTree.node v l r
+    | BinaryTree.node lv ll lr, BinaryTree.leaf rv =>
+      if v > lv then BinaryTree.node lv (min_bubble_down (BinaryTree.node v ll lr)) r
+      else if v > rv then BinaryTree.node rv l (BinaryTree.leaf v)
+      else BinaryTree.node v l r
+    | BinaryTree.node lv ll lr, BinaryTree.node rv rl rr =>
+      if v > lv && lv < rv then BinaryTree.node lv (min_bubble_down (BinaryTree.node v ll lr)) r
+      else if v > lv && lv > rv then BinaryTree.node rv l (min_bubble_down (BinaryTree.node v rl rr))
+      else BinaryTree.node v l r
 
-/-- 下溢：调整堆使父节点值小于子节点值 -/
-def bubble_down : BinaryTree α → BinaryTree α
+def change_to_min_heap :BinaryTree Nat → BinaryTree Nat
+  | BinaryTree.empty => BinaryTree.empty
   | BinaryTree.leaf v => BinaryTree.leaf v
-  | BinaryTree.left v l =>
-    if v > get_value l then BinaryTree.left (get_value l) (bubble_down (BinaryTree.leaf v))
-    else BinaryTree.left v l
-  | BinaryTree.right v r =>
-    if v > get_value r then BinaryTree.right (get_value r) (bubble_down (BinaryTree.leaf v))
-    else BinaryTree.right v r
   | BinaryTree.node v l r =>
-    if get_value l <= get_value r && v > get_value l then
-      BinaryTree.node (get_value l) (bubble_down (BinaryTree.leaf v)) r
-    else if get_value r < get_value l && v > get_value r then
-      BinaryTree.node (get_value r) l (bubble_down (BinaryTree.leaf v))
-    else BinaryTree.node v l r
+    let l_fixed := change_to_min_heap l
+    let r_fixed := change_to_min_heap r
+    let updated := BinaryTree.node v l_fixed r_fixed
+    min_bubble_down updated
 
-/-- 插入一个元素到小顶堆 -/
-def insert (x : α) (h : BinaryTree α) : BinaryTree α :=
-  bubble_up (BinaryTree.node x h (BinaryTree.leaf x))
-
-/-- 弹出小顶堆的最小值 -/
-def pop_min : BinaryTree α → Option (α × BinaryTree α)
-  | BinaryTree.leaf v => some (v, BinaryTree.leaf v)
-  | BinaryTree.node v l r =>
-    let new_heap := bubble_down (BinaryTree.node v l r)
-    some (v, new_heap)
-  | _ => none
-
-end BinaryTree
-
--- 示例小顶堆
+-- Testing the functions with an example
 open BinaryTree
 
-def exampleHeap1 : BinaryTree Nat :=
+def exampleTree : BinaryTree Nat :=
+  node 100
+    (node 2
+      (node 11 (leaf 8) (leaf 23))
+      (node 55 (leaf 12) (leaf 131)))
+    (node 25
+      (node 6 (leaf 13) (leaf 134))
+      (node 4 (leaf 145) (leaf 153)))
+
+def exampleTree2 : BinaryTree Nat :=
+  empty
+
+def exampleTree3 : BinaryTree Nat :=
   node 1
-    (node 2 (leaf 4) (leaf 5))
-    (node 3 (leaf 6) (leaf 7))
+    empty (leaf 2)
 
-def exampleHeap2 : BinaryTree Nat :=
-  node 8
-    (node 9 (leaf 10) (leaf 11))
-    (node 12 (leaf 13) (leaf 14))
+def exampleArray : Array Nat := #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
--- 测试代码
-#eval size exampleHeap1
-#eval depth exampleHeap1
-#eval is_min_heap exampleHeap1
-#eval merge exampleHeap1 exampleHeap2
-#eval insert 0 exampleHeap1
-#eval pop_min exampleHeap1
+-- Original tests
+#eval array_to_min_heap exampleArray
+#eval remove_last_node (array_to_min_heap exampleArray)
+#eval (insert_into_binary_tree 1 (array_to_min_heap exampleArray))
+#eval is_min_heap (insert_into_binary_tree 1 (array_to_min_heap exampleArray))
+#eval is_min_heap (change_to_min_heap (insert_into_binary_tree 1 (array_to_min_heap exampleArray)))
+#eval is_min_heap exampleTree
+#eval change_to_min_heap (exampleTree)
+#eval is_min_heap ((change_to_min_heap (exampleTree)))
+#eval is_complete_binary_tree exampleTree3
+
+-- New tests for added functions
+#eval get_max_value exampleTree 
+#eval invert_binary_tree exampleTree 
+#eval postorder exampleTree 
+#eval inorder exampleTree 
+#eval count_leaves exampleTree 
+#eval mirror exampleTree 
+#eval count_nodes exampleTree
+#eval flatten_to_list exampleTree 
+#eval find_all_paths exampleTree 
+#eval diameter exampleTree 
+#eval sum_of_path_lengths exampleTree 
